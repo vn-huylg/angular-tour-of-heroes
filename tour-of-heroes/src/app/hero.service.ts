853641215e1c5044c506378094e17638501acc5f
vn-huylg <huylg@rakumo.vn>
2021-03-24 10:04:23 +0700

    1 commit

---
+++ tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+@Injectable({ providedIn: 'root' })
+export class HeroService {
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return this.newMethod(id);
+  }
+
+  private newMethod(id: number): Observable<Hero> {
+    return of(HEROES.find(hero => hero.id === id));
+  }
+}
